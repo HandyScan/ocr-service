031272c77559ed901acfa286204f0a8d484a9464
MSPawanRanjith <pawanranjith@gmail.com>
2022-12-09 16:31:02 -0700

    DCSC-14 kafka conection done with ssl

--- service_worker.py
+++ service_worker.py
@@ -9,7 +9,7 @@ import redis
 import jsonpickle
 import hashlib
 from minio import Minio
-from confluent_kafka import Producer
+from confluent_kafka import Producer, Consumer
 
 # intialize the application
 app = Flask(__name__)
@@ -20,9 +20,51 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 kafka_bootstrap_server = os.environ.get('KAFKA_BOOTSTRAP', 'pkc-4r087.us-west2.gcp.confluent.cloud:9092')
-p=Producer({'bootstrap.servers': kafka_bootstrap_server})
+kafka_api_key = os.environ.get('KAFKA_API_KEY', '4BRDEW6OW6Z35KZK')
+kafka_secret_key = os.environ.get('KAFKA_SECRET_KEY', '')
+kafka_config = {
+    'bootstrap.servers':'pkc-4r087.us-west2.gcp.confluent.cloud:9092',
+    'security.protocol':'SASL_SSL',
+    'sasl.mechanisms':'PLAIN',
+    'sasl.username': kafka_api_key,
+    'sasl.password': kafka_secret_key,
+    'group.id':'ocr-consumer',
+    'auto.offset.reset':'earliest'
+}
+
+producer = Producer(kafka_config)
+consumer =  Consumer(kafka_config)
+
 print("Connected to kafka")
 
+minioHost = os.getenv("MINIO_HOST") or "127.0.0.1:9000"
+minioUser = os.getenv("MINIO_USER") or "minioadmin"
+minioPasswd = os.getenv("MINIO_PASSWD") or "minioadmin"
+print(f"Getting minio connection now for host {minioHost}!")
+
+minio_client = None
+try:
+    minio_client = Minio(minioHost, access_key=minioUser, secret_key=minioPasswd, secure=False)
+    print("Got minio connection",minio_client )
+except Exception as exp:
+    print(f"Exception raised in worker loop: {str(exp)}")
+
+def get_details_from_kafka():
+    consumer.subscribe(['ocr_topic'])
+    file_detail_msg = consumer.poll(1.0)
+    if file_detail_msg is None:
+        return
+    if file_detail_msg.error():
+        logger.error('ERROR: Failed to get file details from OCR_TOPIC', str(file_detail_msg.error()))
+        return
+    file_detail = file_detail_msg.value().decode('utf-8')
+    logger.info("Fetched File to process: {}", file_detail)
+    return file_detail
+
+while True:
+    get_details_from_kafka()
+# def get_file_from_minio(file_details):
+#     file_details
 
 # def pdfToText(file_name):
 #     pdfFile = wi(filename = file_name, resolution = 300)
